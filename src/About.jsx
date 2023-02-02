19be1d6dce68f408bea31c79474d31b67de31ec2
JuanMa <mitild82@gmail.com>
2023-02-02 11:30:08 +0100

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className='article'>
       <h2 className='article-title'>About</h2>
-      <p className='article-text'>I am a Frontend Developer recently graduated from the Scrimba School and currently studying a specialty in React JS at the Barcelona IT Academy. With experience as a Graphic Designer and Wordpress developer</p>
+      <p className='article-text'>I am a Frontend Developer recently graduated from the Scrimba School and currently studying a specialty in React JS at the Barcelona IT Academy. With a Graphic Designer and Wordpress developer background</p>
     </div>
   )
 }
